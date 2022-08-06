aea4dd68bc45ec9cce6df8ab8ed8edf2e7ea68df
Everaldo Lima <jlima788@yahoo.com.br>
2022-08-06 02:20:47 -0300

    Incluindo terraform

--- main.tf
+++ main.tf
@@ -14,22 +14,22 @@ provider "digitalocean" {
 resource "digitalocean_kubernetes_cluster" "k8s_iniciativa" {
   name   = var.k8s_name
   region = var.region
-  version = "1.22.8-do.1"
+  version = "1.23.9-do.0"
 
   node_pool {
     name       = "default"
-    size       = "s-2vcpu-4gb"
-    node_count = 2
+    size       = "s-2vcpu-2gb"
+    node_count = 3
   }
 }
 
-resource "digitalocean_kubernetes_node_pool" "premium" {
-  cluster_id = digitalocean_kubernetes_cluster.k8s_iniciativa.id
+# resource "digitalocean_kubernetes_node_pool" "premium" {
+#   cluster_id = digitalocean_kubernetes_cluster.k8s_iniciativa.id
 
-  name       = "premium"
-  size       = "s-4vcpu-8gb"
-  node_count = 2
-}
+#   name       = "premium"
+#   size       = "s-4vcpu-8gb"
+#   node_count = 2
+# }
 
 variable "do_token" {}
 variable "k8s_name" {}
